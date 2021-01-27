aade653f41263501601cb3b3f720dd137daac76d
MikolajKarpeta <72729638+MikolajKarpeta@users.noreply.github.com>
2021-01-27 21:04:21 +0100

    Add files via upload

---
+++ LAB5/include/Macierz.h
@@ -0,0 +1,44 @@
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#ifndef JIPP3V2_MACIERZ_H
+#define JIPP3V2_MACIERZ_H
+
+
+class Macierz {
+protected:
+    int wiersze;
+    int kolumny;
+    double **macierz1 = nullptr;
+public:
+    Macierz(int i, int j);
+    Macierz(int c);
+
+    int wiersze_tab();
+    int  kolumny_tab();
+
+    void set(int n, int m, double val);
+    double get(int n, int m);
+
+    Macierz add(Macierz macierz2);
+    Macierz subtrack(Macierz macierz2);
+    Macierz multyply(Macierz macierz2);
+
+    void print();
+
+    void store();
+    Macierz operator+(Macierz macierz2);
+    Macierz operator-(Macierz macierz2);
+    Macierz operator*(Macierz macierz2);
+    bool operator==(Macierz m2);
+    bool operator!=(Macierz m2);
+    void operator++();
+    void operator--();
+
+
+};
+
+
+#endif //JIPP3V2_MACIERZ_H
